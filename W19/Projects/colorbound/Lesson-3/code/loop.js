3eb4ce0ee9148e845ea68ba66a0b63bb644a44d6
Apaar Madan <apaar_madan@hotmail.com>
2019-03-05 01:32:10 -0500

    Lesson 3 code

--- W19/Projects/colorbound/Lesson-3/code/loop.js
+++ W19/Projects/colorbound/Lesson-3/code/loop.js
@@ -1,96 +1,19 @@
-let input = {
-    left: false,
-    right: false,
-    jump: false
-};
-
 let canvas = document.getElementById("view");
 let ctx = canvas.getContext("2d");
 
-let playerImage = loadImage("assets/player.png");
-
-let player = {
-    sprite: createSprite({
-        image: playerImage,
-
-        frameWidth: 128,
-        frameHeight: 128,
-
-        anims: {
-            run: {
-                startFrame: 6,
-                length: 7,
-                frameTime: 0.08
-            }
-        }
-    }),
-
-    dx: 0,
-    dy: 0
-};
-
-player.sprite.x = 100;
-player.sprite.y = 200;
-
-playAnim(player.sprite, "run");
-
-window.addEventListener("keydown", function(e) {
-    if(e.key == "a") {
-        input.left = true;
-    } else if(e.key == "d") {
-        input.right = true;
-    } else if(e.key == "w") {
-        input.jump = true;
-    }
-});
-
-window.addEventListener("keyup", function(e) {
-    if(e.key == "a") {
-        input.left = false;
-    } else if(e.key == "d") {
-        input.right = false;
-    } else if(e.key == "w") {
-        input.jump = false;
-    }
-});
-
-function processInput() {
-    player.grounded = collideTileMap(player.sprite.x, player.sprite.y + 1, 128, 128);
-
-    if(input.left) {
-        player.dx = -4;
-        player.sprite.flip = true;
-    } else if(input.right) {
-        player.dx = 4;
-        player.sprite.flip = false;
-    } else {
-        player.dx = 0;
-    } 
-
-    if(input.jump && player.grounded) {
-        player.dy = -10;
+function init() {
+    for(let i = 0; i < 20; ++i) {
+        createEnemy(100 + i * 5, 100);
     }
 }
 
+function processInput() {
+    playerProcessInput();
+}
+
 function update() {
-    player.dy += 0.5;
-
-    if(player.dy > 20) {
-        player.dy = 5;
-    }
-
-    if(!collideTileMap(player.sprite.x + player.dx, player.sprite.y, 128, 128)) {
-        player.sprite.x += player.dx;
-    } else {
-        player.dx = 0;
-    }
-    
-    if(!collideTileMap(player.sprite.x, player.sprite.y + player.dy, 128, 128)) {
-        player.sprite.y += player.dy;
-    } else {
-        player.dy = 0;
-    }
-
+    updatePlayer();
+    updateEnemies();
     updateSprites();
 }
 
@@ -116,4 +39,5 @@ function loop() {
     requestAnimationFrame(loop);
 }
 
+init();
 loop();
